2802fa150b0cf6b34c5afae9011c61c2209eacad
NaveenM-1711 <naveenmonharan17112001@psnacet.edu.in>
2024-08-21 00:39:06 +0530

    updated build

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -23,40 +23,36 @@ const LoginPage = ({ onLogin }) => {
   };
 //
 
-  return (
-    <div className='container'>
-      <div class='log-box'>
-      <div class='row'  >
-        <div class='col-md-6'>
-                <div class='loginpage' >
-                  
-                    <h1>Login</h1>
-                    <form onSubmit={handleSubmit}>
-                      <input
-                        type="email"
-                        placeholder="Enter your email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        class='login-input'
-                      />
-                      <button onClick={handleSubmit}>Login</button>
-                    </form>
-                  <div class="terms-and-services">
-                    <h4>
-                    <p>
-                      <a href='/login'>Terms of Service | Privacy Policy</a>
-                    </p>
-                    </h4>
-                  </div>
-                </div>    
-        </div>
-        <div class='col-md-6'>
-              <img src={logo} alt="Logo" class="login-logo" />
+  
+return (
+  <div className="container">
+    <div className="log-box row">
+      <div className="col-md-6 d-flex align-items-center justify-content-center">
+        <div className="loginpage">
+          <h1>Login</h1>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="email"
+              placeholder="Enter your email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="login-input"
+            />
+            <button type="submit">Login</button>
+          </form>
+          <div className="terms-and-services">
+            <p>
+              <a href="/login">Terms of Service | Privacy Policy</a>
+            </p>
+          </div>
         </div>
       </div>
+      <div className="col-md-6 d-flex align-items-center justify-content-center">
+        <img src={logo} alt="Logo" className="login-logo" />
       </div>
     </div>
-  );
-};
+  </div>
+    );
+  };
 
 export default LoginPage;
