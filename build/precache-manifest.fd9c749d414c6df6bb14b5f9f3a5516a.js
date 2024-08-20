e7158b2c0880372b05eb6b422baa9488cc3cb977
NaveenM-1711 <naveenmonharan17112001@psnacet.edu.in>
2024-08-20 20:39:00 +0530

    Your commit message here

---
+++ build/precache-manifest.fd9c749d414c6df6bb14b5f9f3a5516a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa9a4b7679e49034867670f3647491bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ea6dae8fc6fb3c90d903",
+    "url": "/static/css/main.601a4621.chunk.css"
+  },
+  {
+    "revision": "51d5d82f71d563154455",
+    "url": "/static/js/2.b044c0c9.chunk.js"
+  },
+  {
+    "revision": "ea6dae8fc6fb3c90d903",
+    "url": "/static/js/main.a00b5332.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1a6c78986d483e48bf6a6086aab4f476",
+    "url": "/static/media/bgimage.1a6c7898.jpg"
+  },
+  {
+    "revision": "9997b5cb172babc67bab1fe94fb47889",
+    "url": "/static/media/logo.9997b5cb.png"
+  }
+]);
